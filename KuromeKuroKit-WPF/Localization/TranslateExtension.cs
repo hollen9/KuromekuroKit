904d77458b2674184beeb7f8aacb5503c3cdede6
hollen9 <hollen.tw@gmail.com>
2021-03-29 03:05:19 +0800

    Basic UI and localization

---
+++ KuromeKuroKit-WPF/Localization/TranslateExtension.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Windows.Markup;
+
+namespace KuromeKuroKit_WPF.Localization
+{
+    // You exclude the 'Extension' suffix when using in XAML
+    [ContentProperty("Text")]
+    public class TranslateExtension : System.Windows.Markup.MarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        const string ResourceId = "KuromeKuroKit_WPF.Properties.LocaleRes";
+
+        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(
+            () => new ResourceManager(ResourceId, IntrospectionExtensions.GetTypeInfo(typeof(TranslateExtension)).Assembly));
+
+        public string Text { get; set; }
+
+        public TranslateExtension()
+        {
+            // var app = (GeoPager.App)App.Current;
+            //if (app.CurrentCulture == null)
+            //{
+            //    var cul = GeoPager.Helpers.PreferencesHelper.GetUserDefinedCulture();
+            //    app.CurrentCulture = cul;
+            //}
+            //ci = app.CurrentCulture;
+
+
+                //ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            
+
+            //var assembly = typeof(Properties.UIResources.UIResx).GetTypeInfo().Assembly; // "EmbeddedImages" should be a class in your app
+            //foreach (var res in assembly.GetManifestResourceNames())
+            //{
+            //    System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            //}
+        }
+
+        public TranslateExtension(string text) 
+        {
+            Text = text;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return string.Empty;
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    string.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.",
+                                  Text,
+                                  ResourceId,
+                                  ci.Name),
+                    "Text");
+#else
+                translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
--- KuromeKuroKit-WPF/ViewModels/MainWindowViewModel.cs
+++ KuromeKuroKit-WPF/ViewModels/MainWindowViewModel.cs
@@ -1,65 +1,119 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace KuromeKuroKit_WPF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        private string _logMsg;
+        private string title = "KuromeKuro Kit";
+        private string logMsg;
 
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get { return title; }
+            set { SetProperty(ref title, value); }
         }
 
         public string LogMessage
         {
-            get { return _logMsg; }
-            set { SetProperty(ref _logMsg, value); }
+            get { return logMsg; }
+            set { SetProperty(ref logMsg, value); }
         }
 
+        public string CSGORootFolder { get => csgoRootFolder; set => SetProperty(ref csgoRootFolder, value); }
+        public string PlayerMdlInputPath { get => playerMdlInputPath; set => SetProperty(ref playerMdlInputPath, value); }
+
+        public ICommand BrowseCSGORootCommand => new DelegateCommand(() =>
+        {
+            do
+            {
+                var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+                bool? dialogR = dialog.ShowDialog();
+                if (!dialogR.HasValue)
+                {
+                    MessageBox.Show(Strings._Msg_AnErrorOccurred);
+                    return;
+                }
+                if (dialogR.Value)
+                {
+                    string assumingPath = dialog.SelectedPath;
+                    if (!File.Exists(Path.Combine(assumingPath, "csgo.exe")))
+                    {
+                        MessageBox.Show(Strings._Msg_CSGONotFounded);
+                        continue;
+                    }
+                    CSGORootFolder = assumingPath;
+                }
+                break;
+            } while (true);
+        });
+
+        public ICommand BrowsePlayerMdlCommand => new DelegateCommand(() =>
+        {
+            do
+            {
+                var dialog = new Ookii.Dialogs.Wpf.VistaOpenFileDialog();
+                dialog.Multiselect = false;
+                bool? dialogR = dialog.ShowDialog();
+                if (!dialogR.HasValue)
+                {
+                    MessageBox.Show(Strings._Msg_AnErrorOccurred);
+                    return;
+                }
+                if (dialogR.Value)
+                {
+                    string assumingPath = dialog.FileName;
+                    PlayerMdlInputPath = assumingPath;
+                }
+                break;
+            } while (true);
+        });
+
         private bool isDecompiledCompleted;
+        private string csgoRootFolder;
+        private string playerMdlInputPath;
 
         public MainWindowViewModel()
         {
-            isDecompiledCompleted = false;
+            //isDecompiledCompleted = false;
 
-            var cbApp = new Crowbar.App();
-            Crowbar.Decompiler cbDe = new Crowbar.Decompiler();
+            //var cbApp = new Crowbar.App();
+            //Crowbar.Decompiler cbDe = new Crowbar.Decompiler();
 
-            cbDe.CrowbarApp = cbApp;
-            cbApp.Settings = new Crowbar.AppSettings();
-            cbApp.Settings.SetDefaultDecompileReCreateFilesOptions();
-            cbApp.Settings.DecompileMdlPathFileName = @"J:\Output\DOTNET_TEST_HOMURA\.COMPILED\models\player\custom_player\legacy\kuromekuro\homura\homura.mdl";
-            cbApp.Settings.DecompileOutputFolderOption = Crowbar.AppEnums.DecompileOutputPathOptions.WorkFolder;
-            cbApp.Settings.DecompileOutputFullPath = @"J:\Output\dotnet";
+            //cbDe.CrowbarApp = cbApp;
+            //cbApp.Settings = new Crowbar.AppSettings();
+            //cbApp.Settings.SetDefaultDecompileReCreateFilesOptions();
+            //cbApp.Settings.DecompileMdlPathFileName = @"J:\Output\DOTNET_TEST_HOMURA\.COMPILED\models\player\custom_player\legacy\kuromekuro\homura\homura.mdl";
+            //cbApp.Settings.DecompileOutputFolderOption = Crowbar.AppEnums.DecompileOutputPathOptions.WorkFolder;
+            //cbApp.Settings.DecompileOutputFullPath = @"J:\Output\dotnet";
 
-            cbDe.ProgressChanged += (s, e) => 
-            {
-                LogMessage = e.ProgressPercentage + "% ..\r\n" + LogMessage;
-            };
+            //cbDe.ProgressChanged += (s, e) => 
+            //{
+            //    LogMessage = e.ProgressPercentage + "% ..\r\n" + LogMessage;
+            //};
 
-            cbDe.RunWorkerCompleted += (s, e) =>
-            {
-                LogMessage = "Completed! Result: " + e.Result.ToString() + " #\r\n" + LogMessage;
-                isDecompiledCompleted = true;
-            };
+            //cbDe.RunWorkerCompleted += (s, e) =>
+            //{
+            //    LogMessage = "Completed! Result: " + e.Result.ToString() + " #\r\n" + LogMessage;
+            //    isDecompiledCompleted = true;
+            //};
 
-            cbDe.Run();
+            //cbDe.Run();
 
-            Task.Run(() => 
-            {
-                while (!isDecompiledCompleted) 
-                {
-                    Task.Delay(1000);
-                }
-                MessageBox.Show("Completed!");
-            });
+            //Task.Run(() => 
+            //{
+            //    while (!isDecompiledCompleted) 
+            //    {
+            //        Task.Delay(1000);
+            //    }
+            //    MessageBox.Show("Completed!");
+            //});
         }
 
-        
+
     }
 }
